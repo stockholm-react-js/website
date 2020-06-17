2e0dbe732e28e35ed74e689c72a9248cd2e88d2e
Joakim Roos <joakim.roos@student.kyh.se>
2020-06-17 14:04:40 +0200

    install styled-components + tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        //boolean to set if classname prefix should have its element and component-name.
+        displayName: true,
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
