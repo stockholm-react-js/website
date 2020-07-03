77ba987280c00bcb08b22785eaed4eacf9f2e30c
Joakim Roos <joakim.roos@student.kyh.se>
2020-07-03 22:46:29 +0200

    removed comma

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    /* `gatsby-plugin-postcss` */,
+    /* `gatsby-plugin-postcss` */
     {
       resolve: `gatsby-source-filesystem`,
       options: {
